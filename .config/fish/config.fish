863181e23f5783ad90093ac24caeee4c882051ce
GHKEN <tetsuya@ghken.com>
2015-07-09 17:11:28 +0900

    source使えなかった

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,4 +5,4 @@ setenv LS_COLORS "ow=00"
 alias ggpull='git pull origin (git_current_branch)'
 alias ggpush='git push origin (git_current_branch)'
 
-source ~/.config/fish/local.fish
+eval (cat ~/.config/fish/local.fish)
