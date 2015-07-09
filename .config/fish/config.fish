d7739562ae66a6a0749c1f27188588d6db9a5f9d
GHKEN <tetsuya@ghken.com>
2015-07-09 17:16:42 +0900

    外部設定ファイルとかいいかんじに読み込めないの

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,4 +5,6 @@ setenv LS_COLORS "ow=00"
 alias ggpull='git pull origin (git_current_branch)'
 alias ggpush='git push origin (git_current_branch)'
 
-eval (cat ~/.config/fish/local.fish)
+for setting in (cat ~/.config/fish/local.fish)
+    eval $setting
+end
