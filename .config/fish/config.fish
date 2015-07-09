6875ae83ac445dd7129616b45eb9e292d7662ef0
GHKEN <tetsuya@ghken.com>
2015-07-09 14:34:58 +0900

    ggpush, ggpush追加

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,6 @@
 setenv LANG ja_JP.UTF-8
 setenv TERM xterm-256color
 setenv LS_COLORS "ow=00"
+
+alias ggpull='git pull origin '(git_current_branch)
+alias ggpush='git push origin '(git_current_branch)
