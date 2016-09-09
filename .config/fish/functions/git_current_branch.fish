46ded0814d053492ff59577220b05eb8b2c66d09
tetsuya-fukuda <tetsuya.fukuda@gree.co.jp>
2016-09-09 14:02:53 +0900

    fishやめる

--- .config/fish/functions/git_current_branch.fish
+++
@@ -1,4 +0,0 @@
-function git_current_branch
-    set br (git rev-parse --abbrev-ref HEAD)
-    echo $br
-end
--- .config/fish/functions/git_prompt.fish
+++
@@ -1,8 +0,0 @@
-function git_prompt
-    if test -z (git rev-parse --git-dir 2> /dev/null)
-        echo ''
-        return
-    end
-    set st (git status -s)
-    echo (set_color 0d0)'('(git_current_branch)') '
-end
